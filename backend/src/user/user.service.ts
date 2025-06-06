4e73cdd2f2c6597348a3e7dece330eb1122db152
ercanakalar <ercanakalar@gmail.com>
2025-06-06 08:40:32 +0300

    fix(Backend-NestJS:): :bug: update profile but check the nick name is belong to the user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,38 +4,41 @@ import { UpdateUser } from './type/user.type';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
   async updateUser(body: UpdateUser) {
-    const existNickName = await this.prisma.user.findUnique({
+    const existNickName = await this.prisma.user.findFirst({
       where: {
-        nickName: body.nickName
-      }
-    })
+        nickName: body.nickName,
+        NOT: {
+          id: body.id,
+        },
+      },
+    });
 
-    if (existNickName) throw new ConflictException("This Nick Name in use!")
+    if (existNickName) throw new ConflictException('This Nick Name in use!');
 
     const updated = await this.prisma.user.update({
       where: {
-        id: body.id
+        id: body.id,
       },
       data: {
         firstName: body.firstName,
         lastName: body.lastName,
         nickName: body.nickName,
-        photo: body.photo
-      }
-    })
-    return updated
+        photo: body.photo,
+      },
+    });
+    return updated;
   }
 
   async getUserById(id: string) {
     const user = await this.prisma.user.findUnique({
       where: {
-        id
+        id,
       },
-    })
+    });
 
-    return user
+    return user;
   }
 }
